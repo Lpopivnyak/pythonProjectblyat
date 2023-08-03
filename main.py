1a3a5ff0c41712b31558501ceb6d00a62e4f2011
lpopivnyak <136169348+lpopivnyak@users.noreply.github.com>
2023-08-03 17:19:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+
+window = QWidget()
+winLbl = QLabel("Ти даун прийомний")
+Button = QPushButton("Тицьнути")
+window.setLayout(line)
+window.show()
+app.exec()
